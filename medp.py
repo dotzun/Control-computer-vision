932eb505f7c20fb1afd8fc0fac2a4afeb683c510
Alexey <afaaanakin9@gmail.com>
2024-12-07 09:07:33 +0300

    Основная программа

--- medp.py
+++ medp.py
@@ -2,8 +2,10 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import pyautogui as pg
-import os
-
+pg.FAILSAFE = True
+import pygetwindow as pgw
+import time
+import math
 
 def get_points(landmark, shape):
     points = []
@@ -14,48 +16,72 @@ def get_points(landmark, shape):
 def palm_size(landmark, shape):
     x1, y1 = landmark[0].x * shape[1], landmark[0].y * shape[0]
     x2, y2 = landmark[5].x * shape[1], landmark[5].y * shape[0]
-    return ((x1 - x2)**2 + (y1 - y2) **2) **.5
+    return ((x1 - x2)**2 + (y1 - y2) **2) ** .5
 
-#создаем детектор
-handsDetector = mp.solutions.hands.Hands()
+active = pgw.getActiveWindow()
+handsDetector = mp.solutions.hands.Hands(static_image_mode=False,
+                   max_num_hands=1,
+                   min_detection_confidence=0.5,
+                   min_tracking_confidence=0.9)
+
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp.solutions.face_mesh.FaceMesh(static_image_mode=False, max_num_faces=1)
 cap = cv2.VideoCapture(0)
-count = 0
-prev_fist = False
+prev_fist = False   
+sensitivity = 120
+x = 0
+y = 0
+x1 = x
+y1 = y
+prev_fist=False
 while(cap.isOpened()):
+    active = pgw.getActiveWindow()
     ret, frame = cap.read()
     if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
         break
     flipped = np.fliplr(frame)
-    # переводим его в формат RGB для распознавания
     flippedRGB = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
-    # Распознаем
+
     results = handsDetector.process(flippedRGB)
-    # Рисуем распознанное, если распозналось
+    results_face = face_mesh.process(flippedRGB)
     if results.multi_hand_landmarks is not None:
-         
-         cv2.drawContours(flippedRGB, [get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)], 0, (255, 0, 0), 2)
-         (x, y), r = cv2.minEnclosingCircle(get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape))
-         ws = palm_size(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
-         print(2 * r / ws)
-         if 2 * r / ws > 1.3:
-             cv2.circle(flippedRGB,(int(x), int(y)), int(r), (0, 0, 255), 2)
-             # кулак разжат
-             prev_fist = False
-         else:
-             cv2.circle(flippedRGB,(int(x), int(y)), int(r), (0, 255, 0), 2)
-             if not prev_fist:
-                 # произошло сжимание
-                count += 1
-                pg.hotkey('win', 'd')
-                 # Сейчас кулак зажат
-                prev_fist = True
-    # Рисуем наш результат в каждом кадре, даже если рука не детектировалась
-    cv2.putText(flippedRGB, str(count), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), thickness=2)
+        if results_face.multi_face_landmarks is not None:
+            for face_landmarks in results_face.multi_face_landmarks:
+                nose = face_landmarks.landmark[0]
+        x1, y1 = x, y
+        time.sleep(0.075)
+        cv2.drawContours(flippedRGB, [get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)], 0, (255, 0, 0), 2)
+        (x, y), r = cv2.minEnclosingCircle(get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape))
+        ws = palm_size(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
+        bf = results.multi_hand_landmarks[0].landmark[12]
+        sf = results.multi_hand_landmarks[0].landmark[4]
+        pf = results.multi_hand_landmarks[0].landmark[8]
+        print(int(x)-int(x1))
+        if "Left" in str(results.multi_handedness[0]):
+            hand = "Left"
+        else:
+            hand = "Right"
+        if 2 * r / ws > 1.35:
+                cv2.circle(flippedRGB,(int(x), int(y)), int(r), (0, 0, 255), 2)
+                prev_fist = False
+                if int(x)-int(x1)<=-sensitivity and hand == 'Right' and abs(int(x)-int(x1))>=sensitivity:
+                    print(0)
+                    pg.hotkey('alt', 'esc')
+                    pgw.getActiveWindow().maximize()
+                    time.sleep(0.45)
+                elif math.hypot((bf.x - sf.x) * flippedRGB.shape[1], (bf.y - sf.y) * flippedRGB.shape[0]) <= 40:
+                    pg.hotkey('win', 'shift', 's')
+                    time.sleep(0.45)
+                elif math.hypot((nose.x - pf.x) * flippedRGB.shape[1], (nose.y - pf.y) * flippedRGB.shape[0]) <= 45:
+                    pg.press('volumemute')
+                    time.sleep(0.45)   
+        else:
+            cv2.circle(flippedRGB,(int(x), int(y)), int(r), (0, 255, 0), 2)
+            if not prev_fist:
+                if active is not None:
+                    active.minimize()
+                    prev_fist = True
+                    time.sleep(0.45)
 
-    # переводим в BGR и показываем результат
-    res_image = cv2.cvtColor(flippedRGB, cv2.COLOR_RGB2BGR)
-    cv2.imshow("Hands", res_image)
-    #print(results.multi_handedness)
-
-# освобождаем ресурсы
 handsDetector.close()
+face_mesh.close()
